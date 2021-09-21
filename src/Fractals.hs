256024cafa0fa2fd10e1960e1921e04d482be232
dankeyy <dankeyy@protonmail.com>
2021-09-21 16:25:24 +0300

    edited out the implicit mandelbrot use in other fractals, been starting to implement the plotting functions. impl still in progress

--- src/Fractals.hs
+++ src/Fractals.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE RankNTypes #-}
-module Mandelbrot where
+module Fractals (  iterateFractal, mandelbrot, burningShip, tricorn ) where
 
 import Data.Complex
 
 
 type Formula a = (Fractional a, RealFloat a) => Complex a -> Complex a -> Complex a -- e.g z' = z^2 + c where z, c, z' are Complex
-
 
 maxIter = 100
 
@@ -15,19 +14,20 @@ mandelbrot z c = z ^ 2 + c
 
 
 burningShip :: Formula a
-burningShip z = mandelbrot (abs (realPart z) :+ abs (imagPart z))
+burningShip z c = (abs (realPart z) :+ abs (imagPart z)) ^ 2 + c
 
 
 tricorn :: Formula a
-tricorn z = mandelbrot $ conjugate z
+tricorn z c = conjugate z ^ 2 + c
 
 
-fractal :: (Fractional a, RealFloat a) => Formula a -> Complex a ->  Int
-fractal f c = iterateFractal f (0 :+ 0) c 0
+-- i think julia sets can be just our regular sets (mandelbrot for the classic ones) given a const c. maybe in a curried form. TODO 
+
+-- julia :: Formula a
+-- julia z _ = z ^ 2 + ((-0.835) :+ (-0.2321))
 
 
 -- returns intensity in correspondence to a point on the complex plane
--- for mandelbrot, tricorn, burning ship we can just pass in the formula. julia sets are a bit trickier. TODO
 iterateFractal :: (Fractional a, RealFloat a) => Formula a -> Complex a -> Complex a -> Int -> Int
 iterateFractal formula z c iter
   | iter > maxIter = 0
--- src/PlotUtils.hs
+++ src/PlotUtils.hs
@@ -1,15 +1,29 @@
 module PlotUtils where
-    
+
+import Data.Complex
 import Graphics.GD
 
+import Fractals ( iterateFractal, mandelbrot )
 
-type VPoint = (Double, Double) -- note that while gd's Point will refer to pixel points, VPoint will refer to points in the view port
+type VPoint = (Double, Double) -- note that while gd's Point will refer to pixel points, VPoint will refer to points in the view port, on the complex plane
 type ViewPort = (VPoint, VPoint) -- the exact window frame on the complex plane we want to observe 
 
 size@(width, height) = (1200, 800) :: Size
 grid = [(x, y) | x <- [0 .. width - 1], y <- [0 .. height - 1]]
 
 
-plot :: ViewPort -> Size -> String -> IO ()
-plot (p1, p2) (w, h) fileName = undefined
+colorBy :: Int -> Color
+colorBy x = rgb (x^2) (2*x) 40 -- idk
+
+
+draw :: VPoint -> Color
+draw (x, y) = colorBy $ iterateFractal mandelbrot (0 :+ 0) (x :+ y) 0
+
+-- pointing from the outside world to a specific point on the complex plane outputing its x,y as a VPoint
+pixelToVPoint :: Point -> ViewPort -> VPoint 
+pixelToVPoint (x, y) (p1, p2) = undefined 
+
+
+-- plot :: ViewPort -> Size -> String -> IO ()
+-- plot (p1, p2) (w, h) fileName = undefined
 
