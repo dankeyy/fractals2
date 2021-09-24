57e197d509cb97262f35a73922fb2de4a2aef436
dankeyy <dankeyy@protonmail.com>
2021-09-24 23:23:06 +0300

    time to stop playing with types, settling on Complex Floats. this should be enough

--- src/Fractals.hs
+++ src/Fractals.hs
@@ -1,34 +1,34 @@
-{-# LANGUAGE RankNTypes #-}
-module Fractals (  iterateFractal, mandelbrot, burningShip, tricorn ) where
+module Fractals (  iterateFractal, mandelbrot, burningShip, tricorn, Formula ) where
 
 import Data.Complex
 
 
-type Formula a = (Fractional a, RealFloat a) => Complex a -> Complex a -> Complex a -- e.g z' = z^2 + c where z, c, z' are Complex
+-- type Formula a = (Fractional a, RealFloat a) => Complex a -> Complex a -> Complex a -- e.g z' = z^2 + c where z, c, z' are Complex
+type Formula = Complex Float -> Complex Float -> Complex Float -- e.g z' = z^2 + c where z, c, z' are Complex
 
 maxIter = 100
 
 
-mandelbrot :: Formula a
+mandelbrot :: Formula
 mandelbrot z c = z ^ 2 + c
 
 
-burningShip :: Formula a
+burningShip :: Formula
 burningShip z c = (abs (realPart z) :+ abs (imagPart z)) ^ 2 + c
 
 
-tricorn :: Formula a
+tricorn :: Formula
 tricorn z c = conjugate z ^ 2 + c
 
 
 -- i think julia sets can be just our regular sets (mandelbrot for the classic ones) given a const c. maybe in a curried form. TODO 
 
--- julia :: Formula a
+-- julia :: Formula
 -- julia z _ = z ^ 2 + ((-0.835) :+ (-0.2321))
 
 
 -- returns intensity in correspondence to a point on the complex plane
-iterateFractal :: (Fractional a, RealFloat a) => Formula a -> Complex a -> Complex a -> Int -> Int
+iterateFractal :: Formula -> Complex Float -> Complex Float -> Int -> Int
 iterateFractal formula z c iter
   | iter > maxIter = 0
   | otherwise = let z' = formula z c in
