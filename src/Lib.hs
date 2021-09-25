8b495e94871994f4beb0d8bc05a484ba6d9d83e2
dankeyy <dankeyy@protonmail.com>
2021-09-25 20:40:20 +0300

    edited for plotting

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,4 +6,4 @@ import PlotUtils
 import Fractals
 
 someFunc :: IO ()
-someFunc =  plot ((-2.0, 2.0), (2.0, -2.0)) size mandelbrot "mandel.png"
+someFunc =  plot ( (-2.0, 2.0), (2.0, -2.0) ) size tricorn "f.png"
